182b3d7d75cd57f12fee2073e5de16f69fefaf2e
Adela-H <68691380+Adela-H@users.noreply.github.com>
2022-10-05 15:41:18 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact</title>
+	<!--bootstrap-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<!--my css-->
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+</head>
+
+<body>
+
+	<div id="page">
+		<header>
+			<nav class="main-navbar">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="despre noi.php">Despre noi</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li class="main-navbar-phone"><a href="#">&#128222;</a></li>
+				</ul>
+			</nav>
+		</header>
+
+
+		<article>
+
+            <div class="container">
+
+				<div class="continut-contact">
+			
+					<div class="row"> 
+						<div class="col col-md-6">
+							<div class="card border-light">
+								<div class="card-header">
+									<h1>Contact</h1>
+								</div>
+								<div class="card-body">
+									<p>Telefon: <a href="#" class="link-success">0752.345.686</a></p>
+									<p>Adresa de mail: <a href="#" class="link-primary">contact@traiestesanatos.ro</a></p>
+									<p>Web: <a href="#" class="link-primary">traiestesanatos.ro</a></p>
+									<p>Adresa: Cladirea Brasov Business Park, Str. Ionescu Crum nr 1, Brasov.</p>
+									<hr class="hr" style="width:600px;"/>
+								</div>
+							
+								<div class="card-body">
+									<h5 class="card-title">Director general</h5>
+									<p class="card-text"><b>Calin Mircescu</b></p>
+									<p>Email: <a href="#" class="link-primary">calin_mircescu@traiestesanatos.ro</a></p>
+									<hr class="hr" style="width:600px;" />
+								</div>
+
+								<div class="card-body">
+									<h5 class="card-title">Nutritionist</h5>
+									<p class="card-text"><b>Ioana Popescu</b></p>
+									<p>Email: <a href="#" class="link-primary">ioana_popescu@traiestesanatos.ro</a></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col col-md-6">
+							<!-- 4:3 aspect ratio -->
+							<div class="card-body">
+								<div class="embed-responsive embed-responsive-4by3">
+									<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.763891229346!2d25.636923115563185!3d45.63547927910339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b35cf70289d679%3A0xb3edc03ceb03e17!2sBra%C8%99ov%20Business%20Park!5e0!3m2!1sen!2sro!4v1664804285764!5m2!1sen!2sro" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+	
+		</article>
+	
+		<footer>
+
+			<section class="sectiune4">
+				<img src="images/logo_footer.jpg" alt="logoGiveForms" class="logo">
+				<form action="#">
+					<p>Aboneaza-te la newsletter!</p>
+
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="d-flex col-lg-4">
+								<input type="text" class="form-control" placeholder="Adresa de e-mail" aria-label="Adresa de e-mail" aria-describedby="btnGroupAddon">
+								<button class="btn btn-primary" type="submit">Trimite</button>
+							</div>
+						</div>
+					</div>
+
+				</form>
+				<p>&copy; Toate drepturile rezervate!</p>
+			</section>
+
+	
+
+		</footer>
+
+	</div>
+
+	<!--bootstrap javascript-->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  
+</body>
